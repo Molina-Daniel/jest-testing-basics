3852d1a68cbb41751714a668746de8684404cb4e
Daniel Molina <dmolcap@gmail.com>
2019-06-16 17:10:09 +0200

    test Falsy and Equal/StrictEqual

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,11 @@
 const functions = require('./functions');
 
+// toBe
 test('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
+// not
 test('Adds 2 + 2 to NOT equal 4', () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
@@ -15,6 +17,20 @@ test('Adds 2 + 2 to NOT equal 4', () => {
 // toBeTruthy matches anything that an if statement treats as true
 // toBeFalsy matches anything that an if statement treats as false
 
+// toBeNull
 test('Should be null', () => {
   expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test('Should be falsy', () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// toBeEqual
+test('User should be Daniel Molina object', () => {
+  expect(functions.createUser()).toStrictEqual({ // "toBe" only compare primitive values (numbs, boolean, etc), to compare arrays and objects we use "toEqual" or "toStrictEqual"
+    firstName: 'Daniel',
+    lastName: 'Molina'
+  });
 });

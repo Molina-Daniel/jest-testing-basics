2f8b26a0b1774aa9475d52a21a1d735b5ca4e05b
Daniel Molina <dmolcap@gmail.com>
2019-06-17 18:28:50 +0200

    lessThan/greaterThan/toMatch/toContain testing

--- functions.test.js
+++ functions.test.js
@@ -27,10 +27,28 @@ test('Should be falsy', () => {
   expect(functions.checkValue(null)).toBeFalsy();
 });
 
-// toBeEqual
+// toBeEqual / toStrictEqual
 test('User should be Daniel Molina object', () => {
   expect(functions.createUser()).toStrictEqual({ // "toBe" only compare primitive values (numbs, boolean, etc), to compare arrays and objects we use "toEqual" or "toStrictEqual"
     firstName: 'Daniel',
     lastName: 'Molina'
   });
+});
+
+// toBeLessThan / toBeGreaterThan / toBeLessThanOrEqual / toBeGreaterThanOrEqual
+test('Should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+// Regex -> toMatch
+test('There is no O in team', () => {
+  expect('team').not.toMatch(/O/i); // the "i" makes the regex case sensitive
+});
+
+// Arrays -> toContain
+test('Admin should be in usernames', () => {
+  usernames = ['john', 'karen', 'admin'];
+  expect(usernames).toContain('admin');
 });
